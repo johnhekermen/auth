6d905c9b4c1cb7f8e1d8f5f88fcac246db233eba
Danilo Rocha <danilo.15dan@hotmail.com>
2020-07-05 13:15:54 -0300

    refactoring

--- server/auth/index.js
+++ server/auth/index.js
@@ -4,53 +4,46 @@ const bcrypt = require('bcryptjs');
 
 const db = require('../db/connection.js');
 const users = db.get('users');
-// users.index('username');
-users.createIndex('username', { unique: true });
+
+users.createIndex('username', { unique: true }); // define an index to collection
 
 const router = express.Router();
 
-const schema = Joi.object().keys({
+const schema = Joi.object().keys({ // validation schema
     username: Joi.string().regex(/(^[a-zA-Z0-9_]+$)/).min(2).max(30).required(), 
     password: Joi.string().min(6).required()
 });
 
 // any router in here is pre-pended with /auth/
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { // http://localhost:5000/auth/
     res.json({
         message: 'locked'
     });
 });
 
-router.post('/signup', (req, res, next) => {
-    // const data = req.body;
-    // console.log(data);
-    const result = Joi.validate(req.body, schema);
-    if (result.error === null){
-        //check database
-        users.findOne({
+router.post('/signup', (req, res, next) => { // http://localhost:5000/auth/signup/
+    const result = Joi.validate(req.body, schema); // validate req body
+    if (result.error === null){ // move on database validation if matches the schema
+        users.findOne({ // check database to see if already have this user
             username: req.body.username
         }).then(user => {
-            if (user) {
-                // already a user in db with this username
+            if (user) { // already a user in db with this username
                 const error = new Error('This username is not OG');
-                next(error);
-            } else {
-                //hash password and insert
+                next(error); // call next route which is error handler
+            } else { //move on to password hashing
                 bcrypt.hash(req.body.password, 12).then(hashedPassword => {
-                    // res.json({ hashedPassword });
-                    // insert the user in database
-                    const newUser = {
+                    const newUser = { // create an user object with username and hashed password
                         username: req.body.username,
                         password: hashedPassword
                     };
-                    users.insert(newUser).then(insertedUser => {
+                    users.insert(newUser).then(insertedUser => { // insert user object to database
+                        delete insertedUser.password;
                         res.json(insertedUser);
                     });
                 });
             }
-            // res.json({ user });
         });
-    } else {
+    } else { // calling next with validation result if theres an error 
         next(result.error);
     }
 });
--- server/index.js
+++ server/index.js
@@ -4,24 +4,24 @@ const volleyball = require('volleyball');
 const app = express();
 
 const auth = require('./auth/index.js');
-app.use(volleyball);
-app.use(express.json());
+app.use(volleyball); // logger for requests and responses
+app.use(express.json()); // json body parser
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // http://localhost:5000/
   res.json({
     message: '🦄🌈✨Hello World! 🌈✨🦄'
   });
 });
 
-app.use('/auth', auth);
+app.use('/auth', auth); // http://localhost:5000/auth/
 
-function notFound(req, res, next) {
+function notFound(req, res, next) { // if the request route didn't match any existent route
   res.status(404);
   const error = new Error('Not Found - ' + req.originalUrl);
   next(error);
 }
 
-function errorHandler(err, req, res, next) {
+function errorHandler(err, req, res, next) { // middleware to handle errors
   res.status(res.statusCode || 500);
   res.json({
     message: err.message,
@@ -32,7 +32,7 @@ function errorHandler(err, req, res, next) {
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; 
 app.listen(port, () => {
   console.log('Listening on port', port);
 });
