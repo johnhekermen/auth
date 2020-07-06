c50df8444429b6294edde31040e38f26480fb603
JohnDev <johncontact.me777@gmail.com>
2020-07-05 22:14:20 -0300

    add client folder

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">Auth</a>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
